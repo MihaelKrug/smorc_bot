222c41f162be89d6e601b6b201f2f147f4c2de1d
pepega drifter <MihaelKrug>
2021-03-01 20:18:16 +0300

    simple_bot functionality added hello

--- simple_bot.py
+++ simple_bot.py
@@ -13,7 +13,18 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    #if message.content.startswith('$hello'):
+    #    await message.channel.send('Hello!')
+    await check_message(message)
+
+
+async def on_greeting(message):
+	await message.channel.send('Hello!')
+
+msg_lib = {'hello':on_greeting}
+
+async def check_message(message):
+	if message.content in list(msg_lib):
+		await msg_lib[message.content](message)
 
 client.run(token)
